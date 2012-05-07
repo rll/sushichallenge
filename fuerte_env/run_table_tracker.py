898c47c5b1be8dcf1c99f8b4451833dd1f58f975
John Schulman <joschu@rhino.(none)>
2012-05-07 15:53:12 -0700

    fix hardcoded path

--- fuerte_env/run_table_tracker.py
+++ fuerte_env/run_table_tracker.py
@@ -1,5 +1,10 @@
 import subprocess
 import fuerte_env
+import roslib;
+roslib.load_manifest("rospack")
+import rospkg
 
+rp = rospkg.RosPack()
+pkg_path = rp.get_path("spinning_tabletop_detection")
 
-subprocess.check_call(["/home/joschu/sushi_all/sushichallenge/spinning_tabletop_detection/bin/test_tracker_ros","input_cloud:=/camera/rgb/points"], env = fuerte_env.make_fuerte_env())
+subprocess.check_call(["%s/bin/test_tracker_ros"%pkg_path,"input_cloud:=/camera/rgb/points"], env = fuerte_env.make_fuerte_env())
