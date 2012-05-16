1c00ee33566c1b941600d46871a54878e4e0af7a
willow <willow@bqv.willowgarage.com>
2012-05-15 19:37:14 -0700

    fix frame

--- spinning_tabletop_detection/src/tracker_ros.cpp
+++ spinning_tabletop_detection/src/tracker_ros.cpp
@@ -34,7 +34,7 @@ void TabletopTrackerROS::callback(const sensor_msgs::PointCloud2& msg) {
 
 void TabletopTrackerROS::updateTransform() {
   tf::StampedTransform stamped_transform;
-  listener.lookupTransform("base_footprint", "/openni_rgb_optical_frame", ros::Time(0), stamped_transform);
+  listener.lookupTransform("/base_footprint", "/head_mount_kinect_rgb_optical_frame", ros::Time(0), stamped_transform);
   transform = toEigenTransform(stamped_transform.asBt());
 }
 
